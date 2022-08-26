0711f2528d43b5fee01b6b2530da9dc70c1f43b4
Rav3nfo <93236837+Rav3nf0@users.noreply.github.com>
2022-08-27 02:42:33 +0530

    Update filling_gaps.py

--- filling_gaps.py
+++ filling_gaps.py
@@ -1,3 +1,8 @@
+# Filling in the Gaps
+# Write a program that finds all files with a given prefix, such as spam001.txt, spam002.txt, and so on, in a single folder and locates any gaps in the
+# numbering (such as if there is a spam001.txt and spam003.txt but no spam002.txt).
+# Have the program rename all the later files to close this gap.
+
 
 import re,os,shutil
 
@@ -39,6 +44,6 @@ def fillgaps(folder,prefix):
             newfile_name = os.path.join(folder, newfile_name)
             shutil.move(oldname, newfile_name)
     
-folder ='C:\\Users\\AADARSH\\Desktop\\bi0s\\random '
+folder =' YourChoice '
 prefix ='spam'
 fillgaps(folder,prefix)
